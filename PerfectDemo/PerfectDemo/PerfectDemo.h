c3afb4953a5d0f70c99b84f4ae3c5a978edfbccd
TywinZhang <zhangicoding@foxmail.com>
2016-05-30 23:33:25 +0800

    perfect server

---
+++ PerfectDemo/PerfectDemo/PerfectDemo.h
@@ -0,0 +1,19 @@
+//
+//  PerfectDemo.h
+//  PerfectDemo
+//
+//  Created by TywinZhang on 16/5/30.
+//  Copyright © 2016年 AiLi.Technology Co. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PerfectDemo.
+FOUNDATION_EXPORT double PerfectDemoVersionNumber;
+
+//! Project version string for PerfectDemo.
+FOUNDATION_EXPORT const unsigned char PerfectDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerfectDemo/PublicHeader.h>
+
+
